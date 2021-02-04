59355bec8ff09401db692949c07df94a8d537065
Cameron Jones <cjones67@byu.edu>
2021-02-03 22:04:11 -0700

    Here it is

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Movie_Assignment_3.Models;
+
+namespace Movie_Assignment_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult enterMovie()
+        {
+            return View();
+        }
+        //This view takes the data entered into the form, saves it in TempStorage, and then displays the confirmation page
+        [HttpPost]
+        public IActionResult enterMovie(Movies movies)
+        {
+            TempStorage.AddMovies(movies);
+            return View("Confirmation", movies);
+        }
+        //In this view I used Linq to make it so that all the movies are displayed unless the title is listed as 
+        //"Independence Day", in which case it will be stored but not displayed, as it shows in the example in the book
+        public IActionResult movieList()
+        {
+            return View(TempStorage.movies.Where(m => m.Title != "Independence Day"));
+        }
+        public IActionResult myPodcasts()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Assignment_3.Models
+{
+    public class Movies
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public Rating Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Lent { get; set; }
+        [MaxLength(25, ErrorMessage ="You can only enter up to 25 characters of Notes")]
+        public string Notes { get; set; }
+    }
+    //This is how the object type "Rating" is configured as an enumeration of the rating options
+    public enum Rating
+    {
+        G,
+        PG,
+        PG13,
+        R
+    }
+}
